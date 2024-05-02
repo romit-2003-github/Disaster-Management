a9731cf09d0f72e8fb2b2b23f5c35277a290ed72
Yuvrajbh <yuvrajbhalla05@gmail.com>
2024-05-02 14:43:05 +0530

    Update HomePage.js

--- client/src/screens/HomePage.js
+++ client/src/screens/HomePage.js
@@ -50,9 +50,9 @@ const HomePage = () => {
             <div className='flex justify-center py-8 md:gap-8 '>
               <div className='grid md:grid-cols-4 grid-cols-2 gap-2'>
                 <img src={companyLogo1} style={{width:'10rem'}} alt='Loading' className='w-20 h-20 mx-8' />
-                <img src={companyLogo2} alt='Loading' className='w-20 h-20 mx-8' />
-                <img src={companyLogo3} alt='Loading' className='w-20 h-20 mx-8' />
-                <img src={companyLogo4} alt='Loading' className='w-20 h-20 mx-8' />
+                <img src={companyLogo2} style={{width:'10rem'}} alt='Loading' className='w-20 h-20 mx-8' />
+                <img src={companyLogo3} style={{width:'10rem'}} alt='Loading' className='w-20 h-20 mx-8' />
+                <img src={companyLogo4} style={{width:'15rem'}} alt='Loading' className='w-20 h-20 mx-8' />
               </div>
             </div>
           </div>
